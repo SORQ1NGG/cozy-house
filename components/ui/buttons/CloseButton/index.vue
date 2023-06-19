d25e1bd999b2c26e1de7595249762c64185d9d21
SORQ1NGG <daniil.sork@gmail.com>
2023-06-19 16:23:37 +0300

    feat: added close button

---
+++ components/ui/buttons/CloseButton/index.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'CloseButton',
+};
+</script>
+
+<script setup>
+const props = defineProps({
+  tag: {
+    type: String,
+    default: 'button',
+  },
+});
+
+</script>
+
+<template>
+  <component
+      :is="props.tag"
+      ref="root"
+      type="button"
+      class="button-close"
+  />
+</template>
+
+<style src="./style.scss" lang="scss" scoped/>
