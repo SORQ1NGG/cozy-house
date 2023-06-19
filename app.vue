be513dcbb627e4d96dcee4aa866b2817b0da84bb
SORQ1NGG <daniil.sork@gmail.com>
2023-06-19 16:15:54 +0300

    chore: update app

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup>
+import DefaultLayout from "./layouts/default";
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="app" class="app">
+    <DefaultLayout>
+      <NuxtPage/>
+    </DefaultLayout>
   </div>
 </template>
